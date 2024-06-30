efb5705c329c20e915f6ef1c56e26d88cbc3a73f
Carlo Dandan <carlodandan.personal@proton.me>
2024-06-30 01:39:02 +0000

    TEST4
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- script1.sh
+++ script1.sh
@@ -4,7 +4,7 @@ if [ -f vendor_boot.img.lz4 ];then
 	lz4 -B6 --content-size -f vendor_boot.img.lz4 vendor_boot.img
 fi
 
-dd if=vendor_boot.img of=vb.img bs=4k count=0 iflag=count_bytes
+dd if=vendor_boot.img of=vb.img bs=4k count= iflag=count_bytes
 
 if [ ! -f phh.pem ];then
     openssl genrsa -f4 -out phh.pem 4096
