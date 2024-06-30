1c1cbe7c4c87ad408f1b05391914873f6a2e96a4
Carlo Dandan <carlodandan.personal@proton.me>
2024-06-30 01:53:37 +0000

    TEST6
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- script2.sh
+++ script2.sh
@@ -3,9 +3,12 @@
 mkdir unpack
 cd unpack
 ../magiskboot unpack ../vendor_boot.img
+ls
 mv vendor_ramdisk_recovery.cpio vendor_ramdisk_recovery.cpio.lz4
+ls
 lz4 -d vendor_ramdisk_recovery.cpio.lz4
-cpio -idv < vendor_ramdisk_recovery.cpio
+ls
+cpio -idmv < vendor_ramdisk_recovery.cpio
 ls
 # Reverse fastbootd ENG mode check
 ../magiskboot hexpatch system/bin/recovery e10313aaf40300aa6ecc009420010034 e10313aaf40300aa6ecc0094 # 20 01 00 35
